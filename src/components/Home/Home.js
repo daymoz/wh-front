d63552a0251eb3a80c2890948e8edd8d81cde94e
Damien Letellier <damienletellier75018@gmail.com>
2019-02-02 15:06:15 +0100

    news

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
 import PageHeadrest from './../PageHeadrest/PageHeadrest';
+import NewsBlock from './../NewsBlock/NewsBlock';
 
 import './Home.scss';
+
+import img2 from './../HomeLatest/img2.png';
+import img_3 from './../HomeLatest/img_3.png';
+import img1 from './../HomeLatest/img1.png';
+import img3 from './../HomeLatest/img3.jpg';
 
 class Home extends Component {
     render() {
@@ -12,7 +18,10 @@ class Home extends Component {
                 <section id="main-content">
                     <h1>Main</h1>
                     <section id="news-part" className="main-left-side">
-                        
+                        <NewsBlock title="LOREM BIDOU BIDOU" />
+                        <NewsBlock title="LOREM BIDOU BIDOU" />
+                        <NewsBlock title="LOREM BIDOU BIDOU" />
+                        <NewsBlock title="LOREM BIDOU BIDOU" />
                     </section>
                 </section>
             </main>
