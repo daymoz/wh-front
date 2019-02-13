89753858cee4217ecb03afbaf30c1afeae8edff3
daymoz <damienletellier75018@gmail.com>
2019-02-13 18:44:59 +0100

    New header

--- src/components/AccountNavbar/AccountNavbar.js
+++ src/components/AccountNavbar/AccountNavbar.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import { Avatar } from '@material-ui/core';
-
+import Login from './../Login/Login';
 
 import './AccountNavbar.scss';
 import defaultAvatar from './../../default_avatar.png';
 
 class AccountNavbar extends Component {
+
+    state = {
+      open: false,
+    };
+
+    handleClickOpen = () => {
+      this.setState({ open: true });
+    };
+
+    handleClose = (value) => {
+      this.setState({ open: value });
+    };
+
     render() {
       return (
           <div id="account">
               <nav id="account-navbar" role="navigation">
                 <Avatar alt='Default Avatar' src={defaultAvatar} className="default-avatar"></Avatar>
-                <Button className="sign-in"><NavLink activeClassName="active" className="nav-bar item" to="/user/login">Se connecter</NavLink></Button>
+                <Button className="sign-in nav-bar item" onClick={this.handleClickOpen}>Se connecter</Button>
                 <Button className="sign-up"><NavLink activeClassName="active" className="nav-bar item" to="/user/singup">S'inscrire</NavLink></Button>
               </nav>
+              <Login open={this.state.open} onClose={this.handleClose} />
           </div>
       );
     }
